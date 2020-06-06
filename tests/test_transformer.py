51e96042203116580375fdb7e83db69db2ea7ff6
m <m@127.0.0.1localhostlocalhost.localdomainlocalhost4localhost4>
2020-06-06 09:35:28 -0400

    FIX: trailing *

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -3,7 +3,7 @@ from dldsl.statement import Statement
 
 
 def test_matmul_transform():
-    op = "Y[i, k] = X[i, j] * W[j, k] * "
+    op = "Y[i, k] = X[i, j] * W[j, k]"
     parse_tree = parse(op)
     result = transform(parse_tree)
     assert isinstance(result, Statement)
